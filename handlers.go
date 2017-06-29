0ea16c40eec2e9e3b233271aa39f2206a5d2182a
xtaci <daniel820313@gmail.com>
2017-06-29 10:49:43 +0800

    update

--- handlers.go
+++ handlers.go
@@ -3,8 +3,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
-	"log"
 	"net/http"
 
 	"github.com/Jeffail/gabs"
@@ -12,7 +10,7 @@ import (
 )
 
 func getGasPriceHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	ret := fmt.Sprintf(`{"jsonrpc":"2.0","id":1,"result":"%v"}`, latestGasPrice.Load())
+	ret := fmt.Sprintf(`{"gasPrice":"%v"}`, latestGasPrice.Load())
 	w.Write([]byte(ret))
 }
 
@@ -24,12 +22,16 @@ func getTransactionCountHandler(w http.ResponseWriter, r *http.Request, ps httpr
 		return
 	}
 
-	resp, err := http.Post(globalConfig.geth,
+	if resp, err := http.Post(globalConfig.geth,
 		"application/json",
-		bytes.NewBufferString(fmt.Sprintf(`{"jsonrpc":"2.0","method":"eth_getTransactionCount","params":["%v","latest"],"id":1}`, value)))
-	if err != nil {
-		log.Println("call:", err)
+		bytes.NewBufferString(fmt.Sprintf(`{"jsonrpc":"2.0","method":"eth_getTransactionCount","params":["%v","latest"],"id":1}`, value))); err == nil {
+		jsonParsed, _ = gabs.ParseJSONBuffer(resp.Body)
+		value, ok = jsonParsed.Path("result").Data().(string)
+		if !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		ret := fmt.Sprintf(`{"count":"%v"}`, value)
+		w.Write([]byte(ret))
 	}
-
-	io.Copy(w, resp.Body)
 }
