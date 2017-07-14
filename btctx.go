48359bc3689ce5a3337fec2f1896ab3b171d0f8d
sean <284068609@qq.com>
2017-07-14 16:25:34 +0800

    add err response

--- btctx.go
+++ btctx.go
@@ -14,19 +14,24 @@ func getBtcTransactions(w http.ResponseWriter, r *http.Request, ps httprouter.Pa
 	var from, to int
 	var ok bool
 	jsonParsed, err := gabs.ParseJSONBuffer(r.Body)
+	tmpl := `{error:%v}`
+
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(fmt.Sprintf(tmpl, err)))
 		return
 	}
 
 	if address, ok = jsonParsed.Path("address").Data().(string); !ok {
 		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(fmt.Sprintf(tmpl, "get address err")))
 		return
 	}
 
 	if exists := jsonParsed.Exists("from"); exists {
 		if from, ok = jsonParsed.Path("from").Data().(int); !ok {
 			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(fmt.Sprintf(tmpl, "get from err")))
 			return
 		}
 	}
@@ -34,12 +39,14 @@ func getBtcTransactions(w http.ResponseWriter, r *http.Request, ps httprouter.Pa
 	if exists := jsonParsed.Exists("to"); exists {
 		if from, ok = jsonParsed.Path("to").Data().(int); !ok {
 			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(fmt.Sprintf(tmpl, "get to err")))
 			return
 		}
 	}
 
 	if from > to {
 		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(fmt.Sprintf(tmpl, "from gt to")))
 		return
 	}
 
@@ -47,12 +54,14 @@ func getBtcTransactions(w http.ResponseWriter, r *http.Request, ps httprouter.Pa
 		defer resp.Body.Close()
 		if bts, err := ioutil.ReadAll(resp.Body); err != nil {
 			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(fmt.Sprintf(tmpl, err)))
 			return
 		} else {
 			w.Write([]byte(bts))
 		}
 	} else {
 		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(fmt.Sprintf(tmpl, err)))
 		return
 	}
 }
